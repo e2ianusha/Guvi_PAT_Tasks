51cb987bd5231f3413b9c768559b50e91150c848
Anusha B T <44541754+e2ianusha@users.noreply.github.com>
2024-03-31 17:44:24 +0530

    Update sp_pagesrc_assignment.py

--- sp_pagesrc_assignment.py
+++ sp_pagesrc_assignment.py
@@ -4,21 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+# Initialise web driver and launch the url
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 
+# login to website using valid credentials
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
 
+# Fetch the Page title
 page_Title = driver.title
 time.sleep(3)
 print("Title:", page_Title)
 
+# Fetch the current url
 current_url = driver.current_url
 time.sleep(3)
 print("current_url:", current_url)
 
+# Extract the entire contents of page into text file
 source = driver.page_source
 time.sleep(3)
 with open("Webpage_task_11.txt", "w", encoding="utf-8") as file:
